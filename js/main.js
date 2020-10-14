fa77f4761f025c7a84cfb0dacf927931315ff997
Laura Munsch <laura.munsch@etu.unistra.fr>
2020-10-14 16:58:30 +0200

    suppression des données

--- js/main.js
+++ js/main.js
@@ -1,15 +1,43 @@
 window.onload = function () {
     'use strict';
 
-    // bouton d'ajout :
-    let btnAjout = document.getElementById('changer-donnees');
+    // boutons :
+    let btnAjout = document.getElementById('ajouter-donnees');
+    let btnSuppr = document.getElementById('supprimer-donnees');
 
     // définition de la source des données :
     let source = './data/small-data.tsv';
+    let source2 = './data/large-data.tsv';
 
     // définition du container :
     let container = d3.select('#container');
     container.classed('striped', true);
+
+    function gestionDonnes(src) {
+        d3.tsv(src).then((data,error) => {
+            if (error) throw error;
+            
+            // on sélectionne les données 
+            let selection = container
+                .selectAll('div')
+                .data(data);
+
+            // exit : gestion de la suppresion
+            selection
+                .exit()
+                .remove();
+            
+            // enter : ajout et update : modification 
+            // on fusionne la sélection avec ce qu'il y avait avant, en affichant le nom
+            selection
+                .enter()
+                .append('div')
+                .merge(selection)
+                .text((d) => {
+                    return d.Name;
+                });
+        });
+    }
 
     // traitement des données :
     // pourrait aussi être écrit : let data = await d3.tsv(source)
@@ -31,27 +59,12 @@ window.onload = function () {
             });
     });
 
-    // au clic sur le bouton, on modifie les données
+    // au clic sur un bouton, on modifie les données 
     btnAjout.addEventListener('click', () => {
-        // on définit une autre source
-        let source2 = './data/large-data.tsv';
+        gestionDonnes(source2);
+    });
 
-        d3.tsv(source2).then((data,error) => {
-            if (error) throw error;
-            
-            // on sélectionne les données 
-            let selection = container
-                .selectAll('div')
-                .data(data);
-            
-            // on fusionne la sélection avec ce qu'il y avait avant, en affichant le nom
-            selection
-                .enter()
-                .append('div')
-                .merge(selection)
-                .text((d) => {
-                    return d.Name;
-                });
-        });
+    btnSuppr.addEventListener('click', () => {
+        gestionDonnes(source);
     });
 };
