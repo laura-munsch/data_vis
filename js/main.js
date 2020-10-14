8238ca8dccd673a3fbcf69cdb9aa075bb01fc0e0
Laura Munsch <laura.munsch@etu.unistra.fr>
2020-10-14 14:55:12 +0200

    gestion des données basique

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 window.onload = function () {
     'use strict';
 
-    //Le code du projet sera inséré ici
+    // définition de la source des données :
+    let source = './data/small-data.tsv';
 
+    // traitement des données :
+    d3.tsv(source).then((data, error) => {
+        // si on a une erreur, on l'affiche
+        if (error) throw error;
+
+        // sinon :
+        console.log(data);
+    });
 };
